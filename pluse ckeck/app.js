a79ebe5859ffe7df36f3deafb727586396015087
Noof <noof.mohammed998@gmail.com>
2021-05-12 15:07:45 +0300

    pluse ckeck Q2

--- pluse ckeck/app.js
+++ pluse ckeck/app.js
@@ -5,16 +5,20 @@ const port = 3000;
 
 const users = ["John", "Mark"];
 
+
+
 app.get("/users", (req, res, next) => {
   res.json(users);
+  next()
 });
 
-const logUsers =(req, res, next) =>{
-   
-    console.log(users)
-    next()
+const logUsers = (req, res, next) => {
+  console.log(users);
+  next();
+};
 
-}
+app.use(logUsers);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
