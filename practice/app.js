78d0d4850473283d91138891d26244b259307b39
Noof <noof.mohammed998@gmail.com>
2021-05-12 18:47:57 +0300

    Q3 @ Q4

--- practice/app.js
+++ practice/app.js
@@ -5,28 +5,45 @@ const port = 3000;
 
 app.use(express.json());
 
-const authRouter = express.Router();
+const userRouter = express.Router();
+const productsRouter = express.Router();
 
 const users = ["John", "Mark"];
 
+const products = ["keyboard", "mouse"]
 // app.get("/users", (req, res, next) => {
 //   res.json(users);
 // });
 
-authRouter.post("/create", (req, res, next) => {
+userRouter.post("/create", (req, res, next) => {
   users.push(req.body.name);
   res.json(users);
   if (req.body) {
     next();
   }
 });
+
+productsRouter.put("/update/:name",(req,res,next) => {
+    console.log(77777777777777)
+    const name = req.params.name;
+    const newProduct = req.body.newProduct;
+    const index = products.indexOf(name)
+    products[index] = newProduct
+    res.json(products)
+
+})
+
 const logBody = (req, res, next) => {
   console.log(req.body);
   //   next();
 };
+app.use(productsRouter)
 
-app.use("/users", authRouter);
+app.use("/users", userRouter);
+app.use("/products",productsRouter)
+
 app.use(logBody);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
